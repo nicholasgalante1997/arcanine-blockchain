3c65920039c58e50ecfd8b1eaf67b65d19a28394
Nick G <nicholasgalante1997@gmail.com>
2023-06-06 01:02:50 -0600

    adds add-tx route

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ const arcanine = new ArcanineBlockchain();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/blockchain', (req, res) => {
     res.status(200).send({ chain: arcanine, len: arcanine.chain.length });
@@ -25,6 +26,17 @@ app.get('/mine', (req, res) => {
     const nonce = arcanine.proofOfWork(index, lastBlockHash, arcanine.currentTransactions);
     const block = arcanine.appendBlock(nonce, lastBlockHash);
     res.status(201).json({ block, msg: 'New Block Mined' });
+})
+
+app.post('/add-transaction', (req, res) => {
+    console.log(req.body);
+    const { recip = null, vendor = null, token = null } = req.body;
+    if (!recip || !vendor || !token) {
+        res.status(501).json({ err: 'invalid msg body' });
+        return;
+    }
+    const blockIndex = arcanine.appendTransaction(vendor, recip, token);
+    res.status(201).json({ status: 'created', blockToBeAdded: blockIndex });
 })
 
 export const server = http.createServer(app);
